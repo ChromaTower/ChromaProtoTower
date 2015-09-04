0f7f45c82909bc380c90a6bb775412d08debdb5e
nicholastodd1 <nicholas.m.todd@gmail.com>
2015-09-04 11:52:29 +1000

    Fixed previous commit

---
+++ Assets/DirLight.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DirLight : MonoBehaviour {
+	public GameObject track;
+	
+	// Update is called once per frame
+	void Update () {
+		// Point at the tracked object
+		transform.LookAt (track.transform.position);
+	}
+}
