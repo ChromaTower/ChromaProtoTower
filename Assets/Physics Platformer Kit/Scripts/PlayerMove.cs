311b226b59aad52f94d7d949dbff653d54c03405
unknown <4925661@AS409-012.ds.swin.edu.au>
2015-09-18 13:06:33 +1000

    Added various sounds and added script

--- Assets/Physics Platformer Kit/Scripts/PlayerMove.cs
+++ Assets/Physics Platformer Kit/Scripts/PlayerMove.cs
@@ -17,6 +17,7 @@ public class PlayerMove : MonoBehaviour
 	public Animator animator;					//object with animation controller on, which you want to animate
 	public AudioClip jumpSound;					//play when jumping
 	public AudioClip landSound;					//play when landing on ground
+	public AudioClip moveSound;
 	
 	//movement
 	public float accel = 70f;					//acceleration/deceleration in air or on the ground
@@ -158,10 +159,12 @@ public class PlayerMove : MonoBehaviour
 		grounded = IsGrounded ();
 		//move, rotate, manage speed
 		characterMotor.MoveTo (walkDirection, curAccel, 0.7f, true);
-		if (rotateSpeed != 0 && turnDirection.magnitude != 0)
-			characterMotor.RotateToDirection (moveDirection , curRotateSpeed * 5, true);
+		if (rotateSpeed != 0 && turnDirection.magnitude != 0) 
+			characterMotor.RotateToDirection (moveDirection, curRotateSpeed * 5, true);
 
-		characterMotor.ManageSpeed (curDecel, maxSpeed + movingObjSpeed.magnitude, true);
+	
+			characterMotor.ManageSpeed (curDecel, maxSpeed + movingObjSpeed.magnitude, true);
+
 		//set animation values
 		if(animator)
 		{
@@ -169,6 +172,21 @@ public class PlayerMove : MonoBehaviour
 			animator.SetBool("Grounded", grounded);
 			animator.SetFloat("YVelocity", GetComponent<Rigidbody>().velocity.y);
 		}
+		//
+		if(moveSound && grounded && Input.GetKeyDown(KeyCode.W) ||Input.GetKeyDown(KeyCode.S)){
+
+			GetComponent<AudioSource>().volume = 1;
+			GetComponent<AudioSource>().clip = moveSound;
+			GetComponent<AudioSource>().Play ();
+			/*
+			if(curAccel == 0){
+				GetComponent<AudioSource>().volume = 1;
+				GetComponent<AudioSource>().clip = moveSound;
+				GetComponent<AudioSource>().Stop ();
+			}
+			*/
+		}
+		
 	}
 	
 	//prevents rigidbody from sliding down slight slopes (read notes in characterMotor class for more info on friction)
@@ -249,7 +267,7 @@ public class PlayerMove : MonoBehaviour
 		//play landing sound
 		if(groundedCount < 0.25 && groundedCount != 0 && !GetComponent<AudioSource>().isPlaying && landSound && GetComponent<Rigidbody>().velocity.y < 1)
 		{
-			GetComponent<AudioSource>().volume = Mathf.Abs(GetComponent<Rigidbody>().velocity.y)/40;
+			GetComponent<AudioSource>().volume = 40;
 			GetComponent<AudioSource>().clip = landSound;
 			GetComponent<AudioSource>().Play ();
 		}
